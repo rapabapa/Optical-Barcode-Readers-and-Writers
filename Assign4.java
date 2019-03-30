01e6f0290d9d1d23fa89e74db637bea38c236a52
rapabapa <45604184+rapabapa@users.noreply.github.com>
2019-03-29 19:52:53 -0700

    Update Assign4.java

--- Assign4.java
+++ Assign4.java
@@ -16,3 +16,22 @@ public class Assign4
    }
 
 }
+
+
+   public interface BarcodeIO {
+   /*public mutator methods to accept BarcodeImage as argument and
+   store it locally for class data member
+   */
+	boolean scan(final BarcodeImage bc);
+	//accpet string as an argument
+	boolean readText(final String test);
+	//generate barcodeImage representing loca data memeber text
+	boolean generateImageFromText();
+	//generate text based on local barCodeImage
+	boolean translateImageToText();
+	//display prints line out text to console
+	void displayTextToConsole();
+	//print out image to console
+	void displayImageToConsole();
+	
+   }
